5181838eb38340183fa2820cf57a5d07b616ab9c
Jesús <jicg151@gmail.com>
2025-06-08 20:19:59 -0400

    Creación de Login
    Se creo el login y se personalizaron las rutas de angular

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () => import('./componentes/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'app',
+        loadComponent: () => import('./componentes/main/main.component').then(c => c.MainComponent)
+    },
+    {
+        path: '',
+        loadComponent: () => import('./componentes/login/login.component').then(m => m.LoginComponent)
+    }
+];
