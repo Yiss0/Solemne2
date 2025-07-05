d4221142c3f97089658e30774da811b85cf5c9a5
Jesús <jicg151@gmail.com>
2025-07-05 03:04:07 -0400

    Feature: Activación de alerta de tareas
    Se activa alerta de tareas que estan proximas a vencer

--- FrontEnd/src/app/componentes/task-alert/task-alert.component.ts
+++ FrontEnd/src/app/componentes/task-alert/task-alert.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-task-alert',
+  standalone: true,
   imports: [],
   templateUrl: './task-alert.component.html',
   styleUrl: './task-alert.component.css'
 })
 export class TaskAlertComponent {
-
+  @Input() cantidad: number = 0;
 }
